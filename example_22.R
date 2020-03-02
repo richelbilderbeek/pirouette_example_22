0d732ad79fd23263661b7642396e27391329df01
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 13:47:00 +0100

    Use of 'pir_save'

--- example_22.R
+++ example_22.R
@@ -10,38 +10,28 @@
 #
 suppressMessages(library(pirouette))
 
-################################################################################
 # Constants
-################################################################################
 is_testing <- is_on_travis()
 example_no <- 22
 rng_seed <- 314
 folder_name <- file.path(paste0("example_", example_no, "_", rng_seed))
 
-################################################################################
 # Create phylogeny
-################################################################################
 set.seed(rng_seed)
 phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
 
-################################################################################
 # Setup pirouette
-################################################################################
 pir_params <- create_std_pir_params(folder_name = folder_name)
 pir_params$twinning_params$sim_twin_tree_fun = create_sim_yule_twin_tree_fun()
-
 if (is_testing) {
   pir_params <- shorten_pir_params(pir_params)
 }
 
-################################################################################
 # Run pirouette
-################################################################################
 pir_out <- pir_run(
   phylogeny,
   pir_params = pir_params
 )
-
 pir_save(
   phylogeny = phylogeny,
   pir_params = pir_params,
