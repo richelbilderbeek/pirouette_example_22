509e43386e00ae74817ac2b19816fe791db6dd62
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-17 11:05:34 +0100

    Run multiple phylogenies

--- example_22.R
+++ example_22.R
@@ -8,36 +8,51 @@
 # is that the twin tree must also be Yule (in the other
 # example, the twin tree is BD)
 #
-suppressMessages(library(pirouette))
+library(pirouette)
+library(beautier)
+library(beastier)
+library(testthat)
+library(ggplot2)
 
 # Constants
 is_testing <- is_on_ci()
 example_no <- 22
 rng_seed <- 314
 folder_name <- paste0("example_", example_no)
+n_phylogenies <- 5 # Number of replicates
+if (is_testing) {
+  n_phylogenies <- 2
+}
 
-# Create phylogeny
-set.seed(rng_seed)
-phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
+# Create phylogenies
+phylogenies <- list()
+for (i in seq_len(n_phylogenies)) {
+  set.seed(314 - 1 + i)
+  phylogenies[[i]] <- create_yule_tree(n_taxa = 6, crown_age = 10)
+}
+expect_equal(length(phylogenies), n_phylogenies)
 
 # Setup pirouette
-pir_params <- create_std_pir_params(
+pir_paramses <- create_std_pir_paramses(
+  n = n_phylogenies,
   folder_name = folder_name
 )
-pir_params$twinning_params$sim_twin_tree_fun = create_sim_yule_twin_tree_fun()
+for (i in seq_along(pir_paramses)) {
+  pir_paramses[[i]]$twinning_params$sim_twin_tree_fun <-
+    create_sim_yule_twin_tree_fun()
+}
 if (is_testing) {
-  pir_params <- shorten_pir_params(pir_params)
+  pir_paramses <- shorten_pir_paramses(pir_paramses)
 }
 
 # Run pirouette
-pir_out <- pir_run(
-  phylogeny,
-  pir_params = pir_params
+pir_outs <- pir_runs(
+  phylogenies = phylogenies,
+  pir_paramses = pir_paramses
 )
-pir_save(
-  phylogeny = phylogeny,
-  pir_params = pir_params,
-  pir_out = pir_out,
-  folder_name = folder_name
-)
+
+# Save summary
+pir_plots(pir_outs) +
+  ggtitle(paste("Number of replicates: ", n_phylogenies)) +
+  ggsave(file.path(folder_name, "errors.png"), width = 7, height = 7)
 
